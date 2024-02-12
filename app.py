b0f8e8e1c30f1f5c76f4e953478d71dc34024e74
a-little-hat <soumyasarkar309@gmail.com>
2024-02-12 16:37:32 +0530

    api updated

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
- 
+from flask import Flask, request
+from langchain.tools import DuckDuckGoSearchRun 
+
 app = Flask(__name__)
+search = DuckDuckGoSearchRun()
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    return "working"
+
+@app.route('/qa')
+def qa():
+    question = request.args.get('q')
+    return search.run(question)
  
 # main driver function
 if __name__ == '__main__':
